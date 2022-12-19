84819ad6f10354c41a807027d1f0c998acbf9179
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-19 13:13:15 -0500

    added notes

--- hash_function.js
+++ hash_function.js
@@ -26,9 +26,12 @@ function hashFunction(key, arrayLength){
     // can have duplicates
 
 function hash(key, arrayLength){
+    // try to keep arrayLength prime as it causes less collisions 
     let total = 0
     let WEIRD_PRIME = 31
+    // prime number in the hash help spread out keys more uniformly 
     for (let i = 0; i < Math.min(key.length, 100); i++){
+        // using min so that we will never go over more than 100 loops
         let char = key[i]
         let value = char.charCodeAt(0) - 96
         total = (total * WEIRD_PRIME + value) % arrayLength
