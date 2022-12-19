1572720d8b0a9aa674795b99219fecbf6a491fc0
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-19 13:24:41 -0500

    adding pseudocode for set and get methods

--- hash_table.js
+++ hash_table.js
@@ -12,8 +12,22 @@ class HashTable {
             // using min so that we will never go over more than 100 loops
             let char = key[i]
             let value = char.charCodeAt(0) - 96
-            total = (total * WEIRD_PRIME + value) % arrayLength
+            total = (total * WEIRD_PRIME + value) % this.keyMap.length
         }
         return total
+    }
+
+    set(key, value){
+        // hash the key
+        // all items will be in a nested structure to allow for separate chaining
+        
+    }
+
+    get(key){
+        // hash the key
+        // go to that index and retrieve the value
+        // use separate chaining by checking the key 
+        // if key not found, return undefined
+        
     }
 }
